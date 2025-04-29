964d3bbaf1afdaebc1b96f726bbf62bd3c946801
Sham1057u <shamanthachar166@gmail.com>
2025-04-29 11:49:14 +0530

    changed into dynamic port

--- src/main/java/com/example/demo/controller/CartController.java
+++ src/main/java/com/example/demo/controller/CartController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5174", allowCredentials = "true")
+@CrossOrigin(origins = "https://sales-savvy-frontend.vercel.app", allowCredentials = "true")
 @RequestMapping("/api/cart")
 public class CartController {
 
