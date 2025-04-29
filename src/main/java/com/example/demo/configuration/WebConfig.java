646303a8d32976e6c9ac61320ab329c6028786d4
Sham1057u <shamanthachar166@gmail.com>
2025-04-29 23:14:02 +0530

    added extra  methods

---
+++ src/main/java/com/example/demo/configuration/WebConfig.java
@@ -0,0 +1,17 @@
+package com.example.demo.configuration;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")  // Allows all endpoints
+                .allowedOrigins("https://sales-savvy-frontend.vercel.app/")  // Replace with your frontend URL
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");  // Allow all headers
+    }
+}
--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +15,7 @@ import com.example.demo.entity.User;
 import com.example.demo.service.UserService;
 
 @RestController
-@CrossOrigin(origins = "https://sales-savvy-frontend.vercel.app" )
+@CrossOrigin(origins = "http://localhost:8080" )
 @RequestMapping("/api/users")
 public class UserController {
 
@@ -23,6 +24,11 @@ public class UserController {
     @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
+    }
+    
+    @GetMapping("/ping")
+    public String message() {
+    	return "server is running";
     }
 
     @PostMapping("/register")
